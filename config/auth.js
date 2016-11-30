f9853fc55ee9a4abae383b23049b5247468371bc
Injoker <amitraj.1996@gmail.com>
2016-11-30 22:23:48 +0530

    files added

---
+++ config/auth.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1746621458901279',
+		'clientSecret': '4a7a8f5b723cff23d9bdc715b68a3619',
+		'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+		 'enableProof': true,
+		 'profileFields': ['id', 'displayName', 'photos', 'email']
+	},
+
+	'googleAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	}
+}
